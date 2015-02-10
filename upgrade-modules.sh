c7ba091e28b547284eb7e402a7e7b6ea9117f76d
Paul Waring <paul.waring@manchester.ac.uk>
2015-02-10 10:11:00 +0000

    Fix whitespace

--- upgrade-modules.sh
+++ upgrade-modules.sh
@@ -4,21 +4,21 @@ upgrade_lib()
 {
   local lib=$1
 
-	echo "Upgrading module: $lib"
-	perl -MCPAN -Mlocal::lib -e "CPAN::install($lib)"
+  echo "Upgrading module: $lib"
+  perl -MCPAN -Mlocal::lib -e "CPAN::install($lib)"
 }
 
 upgrade_all()
 {
   local libs_file=$1
 
-	if [ -f "$libs_file" ]; then
-		while read -r lib
-	  do
-		  if [ ! -z "$lib" ]; then
-			  upgrade_lib $lib
-		  fi
-	  done < "$libs_file"
+  if [ -f "$libs_file" ]; then
+      while read -r lib
+        do
+          if [ ! -z "$lib" ]; then
+            upgrade_lib $lib
+          fi
+      done < "$libs_file"
 	fi
 }
 
